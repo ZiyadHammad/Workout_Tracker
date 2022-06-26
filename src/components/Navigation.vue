60615eed45d4679a0d9cff3de0c7e067dc3235a0
Ziyad Hammad <ziyad.hammad@outlook.com>
2022-06-26 19:11:18 -0400

    pushing

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,14 @@
 <template>
   <header class="bg-at-light-green text-white" >
 
-    <nav class="container py-5 px-4 flex flex-column gap-4 items-center sm:flex-row">
+    <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
       <div class="flex items-center gap-x-4">
-        <img src="../assets/images/dumbbell-light.png" alt="">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="">
         <h1 class="text-lg">Active Tracker</h1>
       </div>
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link class="cursor-pointer" :to="{name: 'Home'}" ></router-link>
+      </ul>
     </nav>
 
   </header>
